ac76c504b03a47e6f534dcd3479c518e02112328
Worldit12 <69303470+Worldit12@users.noreply.github.com>
2020-12-21 09:19:07 +0900

    Update merge_csv.py

--- combine_excel/merge_csv.py
+++ combine_excel/merge_csv.py
@@ -5,12 +5,14 @@ import os
 input_file = r'D:\RnE\combine_excel\test_csv' # csv파일들이 있는 디렉토리 위치
 output_file = r'D:\RnE\combine_excel\result2.csv' # 병합하고 저장하려는 파일명
 
-allFile_list = glob.glob(os.path.join(input_file, 't*')) # glob함수로 sales_로 시작하는 파일들을 모은다
+allFile_list = glob.glob(os.path.join(input_file, 't*')) # glob함수로 't' 로 시작하는 파일들을 모은다
 
 print(allFile_list)
 allData = [] # 읽어 들인 csv파일 내용을 저장할 빈 리스트를 하나 만든다
 
 for file in allFile_list:
+
+    # csv 읽을 때 한글을 읽을 수 있게 cp949로 인코딩
     df = pd.read_csv(file, encoding='cp949') # for구문으로 csv파일들을 읽어 들인다
     allData.append(df) # 빈 리스트에 읽어 들인 내용을 추가한다
 
