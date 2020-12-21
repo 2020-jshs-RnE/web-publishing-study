bdcd89802d16067c6c2638bad0249ab56604b5c9
Worldit12 <69303470+Worldit12@users.noreply.github.com>
2020-12-21 09:19:02 +0900

    Update convert2csv.py

--- combine_excel/convert2csv.py
+++ combine_excel/convert2csv.py
@@ -3,7 +3,7 @@ import tabula
 
 import os, glob
 
-path = r'D:\RnE\combine_excel\test_pdf'
-files = glob.glob(path + '/*.pdf')
+path = r'D:\RnE\combine_excel\test_pdf' # pdf에서 csv로 바꿀 디렉토리 위치 입력
 
+# 디렉토리 내에 있는 pdf를 csv로 바꿈
 tabula.convert_into_by_batch(path, lattice=True, output_format='csv', pages='all')
